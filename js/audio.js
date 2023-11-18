8a55de0abb601f0299eca8a592148cad27b5c272
x <bladeec1ty@protonmail.com>
2023-11-18 13:59:00 +0200

    gtb sg brah

--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,7 @@
 let player;
 
 function onYouTubeIframeAPIReady() {
-    var vid_ids = ['H221MRRgFZs', '8DcdtmUJLzQ', 'cReuQk0pJbI', 'rLiyFaLs8PY', 'uy63_w8gDGQ', 'o0ndkiL5ivU', 'bSgDauqnufI']
+    var vid_ids = ['H221MRRgFZs', '8DcdtmUJLzQ', 'cReuQk0pJbI', 'rLiyFaLs8PY', 'uy63_w8gDGQ', 'o0ndkiL5ivU', 'bSgDauqnufI', 'O6Si3dKB4Co', 'z0SnviQNHxc', 'cuUZxCQXFa8', 'IpiN-RduTUk', '8jIlX94jBzo', 'nLJV45bLlEc', '_C9-Yt2bI78', 'QX_GQvxqf4M', '2KkMyDSrBVI', '9-DQyw_6KFg', 'CMrcYUxBAxc']
     const randvid = vid_ids[Math.floor(Math.random() * vid_ids.length)];
 
     player = new YT.Player('player', {
@@ -20,5 +20,12 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     event.target.playVideo();
-    document.getElementById("songtitle").innerHTML = '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' + player.getVideoData().video_id + '" target="_blank">' + player.getVideoData().title + '</a>';
+    document.getElementById("songtitle").innerHTML = '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' + player.getVideoData().video_id + '" target="_blank">' + player.getVideoData().title + '</a>';    
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = player.getVideoData().title;
+        } else {
+            document.title = 'x';
+        }
+    });
 }
