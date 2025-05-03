c886ad76df4de0b342cc3d2662ce877c2f2427d3
x <bladeec1ty@protonmail.com>
2025-05-04 01:13:41 +0300

    omg update

--- js/audio.js
+++ js/audio.js
@@ -1,55 +1,136 @@
 let player;
-var vid_ids = ['8DcdtmUJLzQ', 'cReuQk0pJbI', 'rLiyFaLs8PY', 'uy63_w8gDGQ', 'o0ndkiL5ivU', 'bSgDauqnufI', 'O6Si3dKB4Co', 'z0SnviQNHxc', 'cuUZxCQXFa8', 'IpiN-RduTUk', '8jIlX94jBzo', 'nLJV45bLlEc', '_C9-Yt2bI78', 'QX_GQvxqf4M', '2KkMyDSrBVI', '9-DQyw_6KFg', 'CMrcYUxBAxc', 'ZN5wLvJeb7Y', 'k2PUV3KwIgY', 'JEVohob5Wxg', 'YbMrRegZ3H0', 'oGPA9k86UcE', 'OWtpoVmry2Q', '7CiT-Eq1ql8', '4bQrqmKCREk', 'ThWF9TzG1GU', '0gBx5IFLLLI', 'h741KCQsWv4', 'fe_RaIPp8fE', 'tqpeQ_Gk5_w', 'RFcTac1ZUxY', 'TxE9EIrKbOg', 'xSV3M3lFv58', 'OKV0M2lEm-s', '-1ibgyEwgFo', '_Vk_tFoi9PM', 'TOYOhGsaHA0', '4Xo6hGLZfCo', 'KTHR7eoUuLY', 'LHOiDImdRl8', 'kjFC7RBV-N4', 'GJEuUrTJ83k', 'I1MIJ3b3DBQ', 'xN-tvjnA3rA', 'wdSrJpwJ7Ls', 'IWbPl1D9C2Y', 'JiwpG284QPk', '94hC5tTK3Es', 'GaWsgE2mCik', '0nKgqxDH7e4', '5rj_j5ZqT00', 'Fzt2c3IWkhQ', 'Zw-A3Phv3zQ', '7mUpFmjc3eM', 'af1r2LpI1rI', 'k9SHS-XbFbY', 'drQg7YjHqrs', 'uK0JJRXGssc', 'C9BW2UuwuVw', '9ZnMrrUNnJ8', 'rjC9s-UKBTQ', 'f3obrUOiKH0', 'kNk-9t6tRug', 'a-AB9R5YKZo', '66eiQkMLC2Q', '_QV12_KGpQ0', 'IjsapjhXmvw', '2CdAd2eTVNY', 'yMhv69vlSZs', 'xFIldh3WKRU']
+var vid_ids = [
+  "8DcdtmUJLzQ",
+  "cReuQk0pJbI",
+  "rLiyFaLs8PY",
+  "uy63_w8gDGQ",
+  "o0ndkiL5ivU",
+  "bSgDauqnufI",
+  "O6Si3dKB4Co",
+  "z0SnviQNHxc",
+  "cuUZxCQXFa8",
+  "IpiN-RduTUk",
+  "8jIlX94jBzo",
+  "nLJV45bLlEc",
+  "_C9-Yt2bI78",
+  "QX_GQvxqf4M",
+  "2KkMyDSrBVI",
+  "9-DQyw_6KFg",
+  "CMrcYUxBAxc",
+  "ZN5wLvJeb7Y",
+  "k2PUV3KwIgY",
+  "JEVohob5Wxg",
+  "YbMrRegZ3H0",
+  "oGPA9k86UcE",
+  "OWtpoVmry2Q",
+  "7CiT-Eq1ql8",
+  "4bQrqmKCREk",
+  "ThWF9TzG1GU",
+  "0gBx5IFLLLI",
+  "h741KCQsWv4",
+  "fe_RaIPp8fE",
+  "tqpeQ_Gk5_w",
+  "RFcTac1ZUxY",
+  "TxE9EIrKbOg",
+  "xSV3M3lFv58",
+  "OKV0M2lEm-s",
+  "-1ibgyEwgFo",
+  "_Vk_tFoi9PM",
+  "TOYOhGsaHA0",
+  "4Xo6hGLZfCo",
+  "KTHR7eoUuLY",
+  "LHOiDImdRl8",
+  "kjFC7RBV-N4",
+  "GJEuUrTJ83k",
+  "I1MIJ3b3DBQ",
+  "xN-tvjnA3rA",
+  "wdSrJpwJ7Ls",
+  "IWbPl1D9C2Y",
+  "JiwpG284QPk",
+  "94hC5tTK3Es",
+  "GaWsgE2mCik",
+  "0nKgqxDH7e4",
+  "5rj_j5ZqT00",
+  "Fzt2c3IWkhQ",
+  "Zw-A3Phv3zQ",
+  "7mUpFmjc3eM",
+  "af1r2LpI1rI",
+  "k9SHS-XbFbY",
+  "drQg7YjHqrs",
+  "uK0JJRXGssc",
+  "C9BW2UuwuVw",
+  "9ZnMrrUNnJ8",
+  "rjC9s-UKBTQ",
+  "f3obrUOiKH0",
+  "kNk-9t6tRug",
+  "a-AB9R5YKZo",
+  "66eiQkMLC2Q",
+  "_QV12_KGpQ0",
+  "IjsapjhXmvw",
+  "2CdAd2eTVNY",
+  "yMhv69vlSZs",
+  "xFIldh3WKRU",
+];
 
 function onYouTubeIframeAPIReady() {
-    const randvid = vid_ids[Math.floor(Math.random() * vid_ids.length)];
+  const randvid = vid_ids[Math.floor(Math.random() * vid_ids.length)];
 
-    player = new YT.Player('player', {
-      height: '0',
-      width: '0',
-      videoId: randvid, 
-      playerVars: {
-        autoplay: 1,
-        fs: 0
-      },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
+  player = new YT.Player("player", {
+    height: "0",
+    width: "0",
+    videoId: randvid,
+    playerVars: {
+      autoplay: 1,
+      fs: 0,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
   });
 }
 
 function onPlayerReady(event) {
-    event.target.playVideo();
-    document.getElementById("songtitle").innerHTML = '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' + player.getVideoData().video_id + '" target="_blank">' + player.getVideoData().title + '</a>';    
-    document.addEventListener('visibilitychange', function() {
-        if (document.hidden) {
-            document.title = player.getVideoData().title;
-        } else {
-            document.title = 'x';
-        }
-    });
+  event.target.playVideo();
+  document.getElementById("songtitle").innerHTML =
+    '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' +
+    player.getVideoData().video_id +
+    '" target="_blank">' +
+    player.getVideoData().title +
+    "</a>";
+  document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+      document.title = player.getVideoData().title;
+    } else {
+      document.title = "x";
+    }
+  });
 }
 
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        Next();
-    }
+  if (event.data == YT.PlayerState.ENDED) {
+    Next();
+  }
 }
 
 function Next() {
   if (player && player.getVideoData) {
-      const randvid = vid_ids[Math.floor(Math.random() * vid_ids.length)];
+    const randvid = vid_ids[Math.floor(Math.random() * vid_ids.length)];
 
-      player.loadVideoById(randvid);
+    player.loadVideoById(randvid);
 
-      player.addEventListener('onStateChange', function (event) {
-        if (event.data == YT.PlayerState.PLAYING) {
-          document.title = player.getVideoData().title;
-          document.getElementById("songtitle").innerHTML = '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' + randvid + '" target="_blank">' + player.getVideoData().title + '</a>';
-        }
+    player.addEventListener("onStateChange", function (event) {
+      if (event.data == YT.PlayerState.PLAYING) {
+        document.title = player.getVideoData().title;
+        document.getElementById("songtitle").innerHTML =
+          '<a title="Lets all love Lain" href="https://www.youtube.com/watch?v=' +
+          randvid +
+          '" target="_blank">' +
+          player.getVideoData().title +
+          "</a>";
+      }
     });
   } else {
-      console.error("Player or getVideoData method not available");
+    console.error("Player or getVideoData method not available");
   }
 }
--- js/rand.js
+++ js/rand.js
@@ -1,19 +1,19 @@
 async function fetchem() {
-    try {
-        const response = await fetch('js/arts.txt');
-        const text = await response.text();
-        const arr = text.split('====').map(art => art.trim());
-        return arr;
-    } catch (error) {
-        return [];
-    }
+  try {
+    const response = await fetch("js/arts.txt");
+    const text = await response.text();
+    const arr = text.split("====").map((art) => art.trim());
+    return arr;
+  } catch (error) {
+    return [];
+  }
 }
 
 async function setem() {
-    const arr = await fetchem();
-    const i = Math.floor(Math.random() * arr.length);
-    const preska = document.getElementById("art");
-    preska.textContent = arr[i];
+  const arr = await fetchem();
+  const i = Math.floor(Math.random() * arr.length);
+  const preska = document.getElementById("art");
+  preska.textContent = arr[i];
 }
 
 setem();
